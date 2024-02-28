adbfe32f2a926a759e49cfb93a14e5036ada1f97
irjakniibos <irzayaumilsyahrar55@gmail.com>
2024-02-28 17:11:18 +0700

    Add project files.

---
+++ coba/coba.cpp
@@ -0,0 +1,51 @@
+// coba.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+int main()
+{
+		int avg;												// numeric avg
+		double f1 = 8e4;										// numeric f1=8e4
+		double d1 = 2e4;										// numeric d1=2e4
+		cout << "bagilah antara 2 variabel berikut" << endl;    // display 'bagilah antara 2 variabel berikut'
+		cout << "nilai f1 =" << f1 << endl;                     // display 'nilai f1' + f1 
+		cout << "nilai d1 =" << d1 << endl;						// display 'nilai d1' + d1 
+		cout << "Maka f1/d1" << endl;                           // display 'maka f1/d1'
+		avg = f1 / d1;											// compute avg = f1/d1
+		cout << "masukan nilai f1 =" << f1 << endl;				// display 'masukan nilai f1 = ' + f1
+		cout << "masukan nilai d1 =" <<d1<< endl;				// display 'masukan nilai d1 = ' + d1
+		cout << "Maka hasilnya adalah = " << avg << endl;		// cout 'maka hasilnya adalah = ' + avg
+																
+						
+
+
+		// sebagai cacatan gunakan int untuk avg dan gunakan double untuk f1 dan d1
+		// jangan lupa diberi jarak 
+		// numeric avg
+		// numeric f1=8e4
+		// numeric d1=2e4
+		// display 'bagilah antara 2 variabel berikut'
+		// display 'nilai f1' + f1 
+		// display 'nilai d1' + d1 
+		// display 'maka f1/d1'
+		// compute avg = f1/d1
+		// display 'masukan nilai f1 = ' + f1
+		// display 'masukan nilai d1 = ' + d1
+		// cout 'maka hasilnya adalah = ' + avg
+		// selesai 
+		// semangat sayangkuu
+		
+
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
